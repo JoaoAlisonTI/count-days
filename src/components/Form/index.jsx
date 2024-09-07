2c83819ebb9cedf3bf36ffbd5f9f617b54d87b65
joaoAlisonTI <joaoalison.ti@gmail.com>
2024-09-07 13:11:09 -0300

    feat: Adicionando o Sonner como toast de notificação ao criar alguma contagem

--- src/components/Form/index.jsx
+++ src/components/Form/index.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react"
+import { Toaster, toast } from 'sonner'
 import { Tag, CalendarSearch} from "lucide-react"
 
 export const Form = (props) => {
@@ -8,7 +9,7 @@ export const Form = (props) => {
 
   const aoSalvar = (evento) => {
     evento.preventDefault()
-    
+    toast.success('Contagem criada com sucesso!')
     props.aoCriar({
       //id: uuidv4(),
       titulo,
@@ -19,21 +20,24 @@ export const Form = (props) => {
   }
   
   return (
-    <form onSubmit={aoSalvar} className="w-full flex flex-col gap-4 mt-6">
+    <>
+      <Toaster richColors position="top-left" />
+      <form onSubmit={aoSalvar} className="w-full flex flex-col gap-4 mt-6">
       <div className="w-full flex items-center gap-2 border-2 rounded-xl border-zinc-800 py-3 px-2 cursor-pointer hover:border-lime-300">
         <label>
           <Tag className="text-zinc-400 text-sm" />
         </label>
-        <input className="w-full outline-0 bg-zinc-950 placeholder-zinc-400 text-base" placeholder="Digite o titulo do momento/evento..." type="text" value={titulo} onChange={evento => setTitulo(evento.target.value)}/>
+        <input className="w-full outline-0 bg-zinc-950 placeholder-zinc-400 text-base" placeholder="Digite o titulo do momento/evento..." type="text" required value={titulo} onChange={evento => setTitulo(evento.target.value)}/>
       </div>
       
       <div className="w-full flex items-center gap-2 border-2 rounded-xl border-zinc-800 py-3 px-2 cursor-pointer hover:border-lime-300">
         <label>
           <CalendarSearch className="text-zinc-400 text-sm" />
         </label>
-        <input className="w-full outline-0 bg-zinc-950 placeholder-zinc-400 text-base" placeholder="Selecione a data da atividade/evento" type="date" value={data} onChange={evento => setData(evento.target.value)} />
+        <input className="w-full outline-0 bg-zinc-950 placeholder-zinc-400 text-base" placeholder="Selecione a data da atividade/evento" type="date" required value={data} onChange={evento => setData(evento.target.value)} />
       </div>
       <button className="mt-1 border-0 decoration-0 text-center p-3 rounded-2xl bg-lime-300 text-zinc-900 text-base font-medium cursor-pointer hover:bg-lime-400">Criar contagem</button>
     </form>
+    </>
   )
 }
